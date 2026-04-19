5c5e17ae5ded740760304afc7a69a9d63a165511
Muhammad Saqib <segmentationfaulter@users.noreply.github.com>
2026-04-19 17:28:26 +0500

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,24 +1,25 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
 
 const global = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.md', base: './src/content/global' }),
-	schema: z.object({
-		title: z.string(),
-		author: z.string(),
-		publishDate: z.coerce.date(),
-		heroImage: z.string(),
-	}),
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/global" }),
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    publishDate: z.coerce.date(),
+    heroImage: z.string(),
+  }),
 });
 
 const palestine = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.md', base: './src/content/palestine' }),
-	schema: z.object({
-		title: z.string(),
-		author: z.string(),
-		publishDate: z.coerce.date(),
-		heroImage: z.string(),
-	}),
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/palestine" }),
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    publishDate: z.coerce.date(),
+    heroImage: z.string(),
+  }),
 });
 
 export const collections = { global, palestine };
